c25c197d130920b0e1aa60912c6b978219524ac6
David Christenson <mail@noctem.xyz>
2017-04-12 23:02:11 -0600

    Run each test 10000 times

--- test_cyrandom.py
+++ test_cyrandom.py
@@ -5,49 +5,60 @@ from unittest import main, TestCase
 import cyrandom
 
 
+ITERATIONS = 10000
+
+
 class TestRNG(TestCase):
-    def test_randrange(self):
-        rand = cyrandom.randrange(700, 3000)
-        self.assertTrue(700 <= rand < 3000)
-        self.assertIsInstance(rand, int)
+    def test_randrange(self, randrange=cyrandom.randrange):
+        for _ in range(ITERATIONS):
+            rand = randrange(700, 3000)
+            self.assertTrue(700 <= rand < 3000)
+            self.assertIsInstance(rand, int)
 
-    def test_randint(self):
-        rand = cyrandom.randint(700, 3000)
-        self.assertTrue(700 <= rand <= 3000)
-        self.assertIsInstance(rand, int)
+    def test_randint(self, randint=cyrandom.randint):
+        for _ in range(ITERATIONS):
+            rand = randint(700, 3000)
+            self.assertTrue(700 <= rand <= 3000)
+            self.assertIsInstance(rand, int)
 
-    def test_choice(self):
+    def test_choice(self, choice=cyrandom.choice):
         seq = (7, 13, 47, 84)
-        rand = cyrandom.choice(seq)
-        self.assertIn(rand, seq)
+        for _ in range(ITERATIONS):
+            rand = choice(seq)
+            self.assertIn(rand, seq)
 
-    def test_shuffle(self):
+    def test_shuffle(self, shuffle=cyrandom.shuffle):
         original = list(range(73, 137))
-        shuffled = original.copy()
-        cyrandom.shuffle(shuffled)
-        self.assertNotEqual(original, shuffled)
-        self.assertIsInstance(shuffled, list)
+        for _ in range(ITERATIONS):
+            shuffled = original.copy()
+            shuffle(shuffled)
+            self.assertNotEqual(original, shuffled)
+            self.assertIsInstance(shuffled, list)
 
-    def test_choices(self):
+    def test_choices(self, choices=cyrandom.choices):
         seq = (6, 8, 10, 12, 16, 24, 32, 48)
-        rand = cyrandom.choices(seq, (4, 38, 73, 84, 88, 96, 99, 100))
-        self.assertIn(rand[0], seq)
-        self.assertIsInstance(rand, list)
+        for _ in range(ITERATIONS):
+            rand = choices(seq, (4, 38, 73, 84, 88, 96, 99, 100))
+            self.assertIn(rand[0], seq)
+            self.assertIsInstance(rand, list)
 
-    def test_choose_weighted(self):
+    def test_choose_weighted(self, choose_weighted=cyrandom.choose_weighted):
         seq = (6, 8, 10, 12, 16, 24, 32, 48)
-        rand = cyrandom.choose_weighted(seq, (4, 38, 73, 84, 88, 96, 99, 100))
-        self.assertIn(rand, seq)
+        for _ in range(ITERATIONS):
+            rand = choose_weighted(seq, (4, 38, 73, 84, 88, 96, 99, 100))
+            self.assertIn(rand, seq)
 
-    def test_uniform(self):
-        rand = cyrandom.uniform(7.1, 11.3)
-        self.assertTrue(7.1 <= rand <= 11.3)
-        self.assertIsInstance(rand, float)
+    def test_uniform(self, uniform=cyrandom.uniform):
+        for _ in range(ITERATIONS):
+            rand = uniform(7.1, 11.3)
+            self.assertTrue(7.1 <= rand <= 11.3)
+            self.assertIsInstance(rand, float)
 
-    def test_triangular(self):
-        rand = cyrandom.triangular(13.7, 47.3, 17.6)
-        self.assertTrue(13.7 <= rand <= 47.3)
-        self.assertIsInstance(rand, float)
+    def test_triangular(self, triangular=cyrandom.triangular):
+        for _ in range(ITERATIONS):
+            rand = triangular(13.7, 47.3, 17.6)
+            self.assertTrue(13.7 <= rand <= 47.3)
+            self.assertIsInstance(rand, float)
 
 
 if __name__ == '__main__':
